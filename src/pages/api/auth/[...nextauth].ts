5489fbb9dd5e5b616d244aeb59adf456b1c97c4c
Flavia Silva <flaviaass65@gmail.com>
2023-03-09 16:39:23 -0300

    Usando FauanDB concluido

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,19 @@
 import { query as q } from 'faunadb'
 
-import NextAuth, { NextAuthOptions} from "next-auth"
+// import NextAuth, { NextAuthOptions} from "next-auth"
+import NextAuth from 'next-auth/next';
+// import Providers from 'next-auth/providers'
+
 import GithubProvider from "next-auth/providers/github"
 
 import { fauna } from '../../../services/fauna';
 
-export const authOptions: NextAuthOptions = ({
+// export const authOptions: NextAuthOptions = ({
+  export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+// Providers.GitHub({
+  
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
@@ -20,18 +26,40 @@ export const authOptions: NextAuthOptions = ({
     }),
     // ...add more providers here
   ],
+  
   callbacks:{
-    async signIn({ user, account, profile, credentials }) {
+    async signIn({ user, account, profile }) {
       const { email } = user
 
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email }}
-        )
-      )
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email!)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email!)
+              )
+            )    
+          )
+        )      
+
       return true
+    } catch {
+      return false
+    }
     },
   }
 })
-export default NextAuth(authOptions)
+// export default NextAuth(authOptions)
