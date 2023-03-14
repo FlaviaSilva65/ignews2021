4ac01f25e753f5c0b2ae7c9b50ebdcab0d8c54ae
Flavia Silva <flaviaass65@gmail.com>
2023-03-14 16:50:21 -0300

    Até o Webhooks do Stripe, não direciona para cadastrar cartão

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,13 @@
-
-import { getStripeJs } from "@/services/stripe-js";
-import { useSession, signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { api } from '../../services/api'
+import { getStripeJs } from "@/services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps){
   const { data: session } = useSession();
 
  async function handleSubscribe() {
@@ -22,10 +22,11 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
       const { sessionId } = response.data;
 
       const stripe = await getStripeJs();
+      await stripe?.redirectToCheckout({ sessionId });
 
-      await stripe.redirectToCheckout({sessionId})
     } catch (err) {
-  alert(err.message);
+      // alert(err.message);
+  
     }
   }
 
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,12 +1,14 @@
 import Stripe from 'stripe';
-import { version } from '../../package.json'
+import packageInfo from '../../package.json'
+
+const version = packageInfo.version;
 
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY!,
   {
     apiVersion: '2022-11-15',
     appInfo: {
-      name: 'Ignews2021',
+      name: 'Ignewsflavia',
       version
     },
   }
